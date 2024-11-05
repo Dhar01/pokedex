d70f22f292bac719faa9218086e69b5b12590691
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-05 22:21:57 +0600

    refactored some tests

--- main_test.go
+++ main_test.go
@@ -9,15 +9,29 @@ func TestCleanInput(t *testing.T) {
 	t.Run("empty string", func(t *testing.T) {
 		got := cleanInput("    ")
 		want := []string{}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkInput(t, got, want)
 	})
 	t.Run("spaces both side", func(t *testing.T) {
 		got := cleanInput("  Hello  ")
 		want := []string{"hello"}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkInput(t, got, want)
 	})
+	t.Run("multiple uppercase letter", func(t *testing.T) {
+		got := cleanInput("HeLP")
+		want := []string{"help"}
+		checkInput(t, got, want)
+	})
+	t.Run("double string type", func(t *testing.T) {
+		got := cleanInput(" HellO World ")
+		want := []string{"hello", "world"}
+		checkInput(t, got, want)
+	})
+
+}
+
+func checkInput(t testing.TB, got, want []string) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
