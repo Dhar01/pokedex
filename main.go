c12a617eb5fbeb6b6c9747b6903eab087c406478
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-05 15:54:34 +0600

    refactoring code

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type cliCommand struct {
 	callback    func() error
 }
 
-func figureCommand() map[string]cliCommand {
+func figureCmd() map[string]cliCommand {
 	return map[string]cliCommand{
 		"help": {
 			name:        "help",
@@ -28,25 +28,21 @@ func figureCommand() map[string]cliCommand {
 }
 
 func main() {
-	// var answer string
 	answer := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
-		// fmt.Scan(&answer)
-		// figure(answer)
 		answer.Scan()
-		// fmt.Println(answer.Text())
 
-		command, exists := figureCommand()[answer.Text()]
+		cmd, exists := figureCmd()[answer.Text()]
 		if exists {
-			err := command.callback()
+			err := cmd.callback()
 			if err != nil {
 				fmt.Println(err)
 			}
 			continue
 		} else {
-			fmt.Println("Unknown command")
+			fmt.Println("Unknown cmd")
 			continue
 		}
 	}
@@ -54,31 +50,17 @@ func main() {
 
 func commandHelp() error {
 	fmt.Println("\nWelcome to the Pokedex!")
-	fmt.Println("Usage:")
+	fmt.Printf("Usage:\n\n")
 
-	for _, command := range figureCommand() {
-		fmt.Printf("%s: %s\n", command.name, command.description)
+	for _, cmd := range figureCmd() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
 
+	fmt.Println()
 	return nil
-	// fmt.Println("commandHelp")
-	// return fmt.Errorf("this is commandHelp")
-
 }
 
 func commandExit() error {
 	os.Exit(0)
 	return nil
-
-	// fmt.Println("commandExit")
-	// return fmt.Errorf("this is commandExit")
 }
-
-// func figure(choice string) {
-// 	if choice == "help" {
-// 		fmt.Println("\nWelcome to the Pokedex!")
-// 		fmt.Println("Usage:")
-// 		fmt.Println("\nhelp: Displays a help message")
-// 		fmt.Println("exit: Exit the Pokedex")
-// 	}
-// }
