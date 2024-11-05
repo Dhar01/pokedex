104060e97f2ba1b283a8b3da53ea5bdff9724b6f
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-05 22:50:44 +0600

    adding run script and refactored

---
+++ commands.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func FigureCmd() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "map of next 20 locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "map of previous 20 locations",
+			callback:    commandMapB,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("\nWelcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+
+	for _, cmd := range FigureCmd() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
+func commandMap() error {
+	return nil
+}
+
+func commandMapB() error {
+	return nil
+}
