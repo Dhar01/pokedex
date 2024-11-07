505e80bb4f608a2a14f45f000df3f29b92c40774
Loknath Dhar <loknath-dhar@pm.me>
2024-11-07 17:25:21 +0600

    adding mapb

--- command_map.go
+++ command_map.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func commandMapF(cfg *config) error {
 	locationsForward, err := cfg.pokeAPIClient.ListLocationAreas(cfg.nextLocationURL)
@@ -11,16 +14,29 @@ func commandMapF(cfg *config) error {
 	cfg.nextLocationURL = locationsForward.Next
 	cfg.previousLocationURL = locationsForward.Previous
 
-	for _, loc := range locationsForward.Results {
-		fmt.Println(loc.Name)
+	for _, location := range locationsForward.Results {
+		fmt.Println(location.Name)
 	}
 
 	return nil
 }
 
 func commandMapB(cfg *config) error {
+	if cfg.previousLocationURL == nil {
+		return errors.New("you're on the first page")
+	}
+
 	locationsBackward, err := cfg.pokeAPIClient.ListLocationAreas(cfg.previousLocationURL)
 	if err != nil {
 		return err
 	}
+
+	cfg.nextLocationURL = locationsBackward.Next
+	cfg.previousLocationURL = locationsBackward.Previous
+
+	for _, location := range locationsBackward.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
 }
