d3d3000da1ec37c368cbc07e9779030279291767
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-09 16:27:08 +0600

    pokecache implemented, tests passed

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -21,11 +21,12 @@ func NewCache(interval time.Duration) Cache {
 		mux:   &sync.Mutex{},
 	}
 
-	c.reapLoop(interval)
+	go c.reapLoop(interval)
 
 	return c
 }
 
+// adding new entry to the cache
 func (c Cache) Add(key string, value []byte) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
@@ -36,6 +37,7 @@ func (c Cache) Add(key string, value []byte) {
 	}
 }
 
+// check if an entry exists or not
 func (c Cache) Get(key string) ([]byte, bool) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
@@ -51,23 +53,20 @@ func (c Cache) reapLoop(interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
-	// for {
-	// 	select {
-	// 	case <-ticker.C:
-	// 		c.entryChecker(time.Now().UTC(), interval)
-	// 	}
-	// }
-
 	for range ticker.C {
 		c.entryChecker(time.Now().UTC(), interval)
 	}
 
 }
 
+// check if an entry is older or not, delete or update
 func (c Cache) entryChecker(now time.Time, last time.Duration) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
 
-	// code to add
-
+	for key, value := range c.cache {
+		if value.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, key)
+		}
+	}
 }
