9425ff58cdc4d4083282013371dc9ba9eb1df72f
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-14 18:59:44 +0600

    getlocation implemented. Search Pokemon!

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -1,9 +1,41 @@
 package pokeapi
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
 func (c *Client) GetLocation(name string) (Location, error) {
 	url := baseURL + "/location-area/" + name
 
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := Location{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return Location{}, err
+		}
+		return locationResp, nil
+	}
+
+	req, err := http.Get(url)
+	if err != nil {
+		return Location{}, err
+	}
+	defer req.Body.Close()
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
 	locationResp := Location{}
+	err = json.Unmarshal(body, &locationResp)
+	if err != nil {
+		return Location{}, err
+	}
+
+	c.cache.Add(url, body)
 
 	return locationResp, nil
 }
